4f036778d24a161e2dce0763f8552f461eafa4b0
Yu Sugihara <yu57th@gmail.com>
2021-03-26 09:26:14 +0900

    v2.2.1

--- qtlseq/qtlplot.py
+++ qtlseq/qtlplot.py
@@ -33,7 +33,8 @@ class QTLplot(object):
             os.mkdir(self.out)
 
     def run_snpEff(self):
-        cmd = 'snpEff ann -s {0}/snpEff_summary.html \
+        cmd = 'snpEff ann -Xmx8g \
+                          -s {0}/snpEff_summary.html \
                           {1} \
                           {2} \
                           1> {0}/qtlseq.snpEff.vcf \
